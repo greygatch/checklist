238f81425d8d650057955a8cf55071d11884fe9f
Evan Gatchell <greygatch@gmail.com>
2015-04-23 11:39:33 -0700

    navbar and setup

--- client/index.js
+++ client/index.js
@@ -1,1 +1,9 @@
-angular.module('poseidon', ['firebase']);
+'use strict';
+
+angular.module('checklist', ['firebase', 'ui.router'])
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  .state('home', {url: '/', templateUrl: '/views/home/home.html'});
+}]);
